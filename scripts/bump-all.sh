ef470dbb8bc412c627071fae807a887727ddbace
tajo <vojtech@miksu.cz>
2023-07-05 02:37:12 -0700

    simplify

--- scripts/bump-all.sh
+++ scripts/bump-all.sh
@@ -1,6 +1,15 @@
 #!/usr/bin/env bash
 set -x
 
+# Get the latest commit message
+commit_message=$(git log --format=%B -n 1)
+
+# Check if the commit message matches the target string
+if [[ "$commit_message" == "Bump npm swc-plugin-fusion" ]]; then
+  echo "Last commit message is 'Bump npm swc-plugin-fusion'. Skipping the job."
+  exit 0
+fi
+
 git config --local --unset-all credential.helper
 git remote remove origin
 git remote add origin https://${GH_TOKEN}@github.com/tajo/swc-plugin-fusion.git
@@ -15,3 +24,5 @@ git add .
 git commit -m "Bump npm swc-plugin-fusion"
 git push origin main
 git push origin main --tags
+npm publish --workspaces
+
