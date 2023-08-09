82f13ab4cf5fde7b1787ff62eea301fdf9ac7e16
Ryan Tsao <rtsao@uber.com>
2023-08-09 14:38:10 -0700

    Ensure generated paths in split() matches JS

--- fixtures/split-no-ssr/output.js
+++ fixtures/split-no-ssr/output.js
@@ -3,6 +3,6 @@ Object.defineProperties(import("./foo/baz"), {
         value: []
     },
     __MODULE_ID: {
-        value: "virtual:fusion-vite-split-loader?importer=%2Fpath%2Fto%2Ffile%2Ejs&specifier=%2E%2Ffoo%2Fbaz"
+        value: "virtual:fusion-vite-split-loader?importer=%2Fpath%2Fto%2Ffile.js&specifier=.%2Ffoo%2Fbaz"
     },
 });
