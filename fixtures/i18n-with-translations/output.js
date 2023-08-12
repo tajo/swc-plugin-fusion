b940475c6f0f2862eaba732f49b60fc1946e3f15
Ryan Tsao <rtsao@uber.com>
2023-08-11 17:14:03 -0700

    Fix composed withTranslations (#13)

--- fixtures/i18n-with-translations/output.js
+++ fixtures/i18n-with-translations/output.js
@@ -1,15 +1,22 @@
 import _fusionPluginI18nChunkTranslationMap from "virtual:fusion-vite-i18n-map";
 import { withTranslations } from 'fusion-plugin-i18n-react';
-export default withTranslations(['test', 'foo'])(({
-  translate
-}) => {
+export default withTranslations([
+  'test',
+  'foo'
+])(({ translate }) => {
   return <input placeholder={translate('test', {
     name: 'world'
   })} />;
 });
+export const Baz = compose(withReducer(baz), withTranslations([
+  'bar'
+])(({ translate }) => {
+  return <input placeholder={translate('bar')} />;
+}));
 _fusionPluginI18nChunkTranslationMap.add("/path/to/file.js", [
   "vite-i18n-chunk"
 ], [
+  "bar",
   "foo",
   "test"
 ]);
